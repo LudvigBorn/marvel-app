8dd0b8da1d10d4144a91ff2bb21e577572d83e06
Bralin Temirlan <ludvig.born@gmail.com>
2023-08-02 23:46:19 +0600

    Updates

---
+++ static/js/346.129302cd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[346],{5346:function(e,t,n){n.r(t);var i=n(9613),o=n(1087),s=n(184);t.default=function(){return console.log("page404"),(0,s.jsxs)("div",{children:[(0,s.jsx)(i.Z,{}),(0,s.jsx)("p",{style:{textAlign:"center",fontWeight:"bold",fontSize:"24px"},children:"Page doesn't exist"}),(0,s.jsx)(o.rU,{style:{textAlign:"center",fontWeight:"bold",fontSize:"24px",marginTop:"30px"},to:"..",children:"Back to main page"})]})}}}]);
+//# sourceMappingURL=346.129302cd.chunk.js.map
